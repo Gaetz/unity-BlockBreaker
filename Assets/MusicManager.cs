f16a70a49128844ed3785ec2918601e760e94422
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-04-28 19:35:09 +0200

    Music manager, as a singleton

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    static MusicManager instance = null;
+
+	// Use this for initialization
+	void Start () {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        } 
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
