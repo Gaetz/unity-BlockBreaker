da9ba4a512b9f0573e070015f2718df9651d1ae2
Gaetz <gaetz@rufflerim.com>
2017-05-01 06:38:16 +0200

    Lose condition

---
+++ Assets/_Code/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager LevelManager;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        LevelManager.LoadLevel("Loss");
+    }
+}
