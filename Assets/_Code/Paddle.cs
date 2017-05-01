5f75a16165886c1d70eb8efa2754c3c49d32d3af
Gaetz <gaetz@rufflerim.com>
2017-05-01 15:28:51 +0200

    Autoplay option

--- Assets/_Code/Paddle.cs
+++ Assets/_Code/Paddle.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
+
+    /// <summary>
+    /// True if computer play alone
+    /// </summary>
+    public bool Autoplay;
 
     const float ScreenWidthInGameUnit = 16f;
 
@@ -13,7 +19,21 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float xPosition = Mathf.Clamp(Input.mousePosition.x / Screen.width * ScreenWidthInGameUnit, 0.5f, 15.5f);
-        transform.position = new Vector3(xPosition, this.transform.position.y, this.transform.position.z);     
+        if (Autoplay)
+            AutoUpdate();
+        else
+            PlayerUpdate();
 	}
+
+    private void PlayerUpdate()
+    {
+        float xPosition = Mathf.Clamp(Input.mousePosition.x / Screen.width * ScreenWidthInGameUnit, 0.5f, 15.5f);
+        transform.position = new Vector3(xPosition, this.transform.position.y, this.transform.position.z);
+    }
+
+    private void AutoUpdate()
+    {
+        float xPosition = GameObject.FindObjectOfType<Ball>().GetComponent<Transform>().position.x;
+        transform.position = new Vector3(xPosition, this.transform.position.y, this.transform.position.z);
+    }
 }
