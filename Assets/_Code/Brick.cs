152393e00f269a540c35ea7a3408d2f181fe317d
Gaetz <gaetz@rufflerim.com>
2017-05-01 13:51:24 +0200

    Add an invicible brick prefab with tag system

--- Assets/_Code/Brick.cs
+++ Assets/_Code/Brick.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-    public int MaxHits;
-    int timesHit;
+    /// <summary>
+    /// Break stages Sprites
+    /// </summary>
+    public Sprite[] HitSprites;
+
+    /// <summary>
+    /// Times the brick has been hit
+    /// </summary>
+    private int timesHit;
+
+    /// <summary>
+    /// Reference to level manager
+    /// </summary>
     LevelManager levelManager;
 
 	// Use this for initialization
@@ -16,16 +28,41 @@ public class Brick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(timesHit >= MaxHits)
-        {
-            Destroy(gameObject);
-        }
+
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        timesHit = timesHit + 1;
+        // Collision effect if brick is breakable
+        if (this.tag == "Breakable")
+            HandleHit();
+        // Win condition
         //Win();
+    }
+
+    void HandleHit()
+    {
+        int maxHits = HitSprites.Length + 1;
+        timesHit = timesHit + 1;
+        // Destruction
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+        // If not destroyed alter sprite
+        else
+        {
+            ChangeSprite();
+        }
+    }
+
+    /// <summary>
+    /// Change sprite when hit
+    /// </summary>
+    private void ChangeSprite()
+    {
+        if(HitSprites[timesHit - 1])
+            GetComponent<SpriteRenderer>().sprite = HitSprites[timesHit - 1];
     }
 
     void Win()
